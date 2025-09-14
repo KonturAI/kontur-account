ed2bfc86d2e81f504572864371fec79f9f0477a3
mm <mmmdev2003@gmail.com>
2025-09-14 19:54:12 +0300

    dev

--- internal/model/client/kontur_authorization.py
+++ internal/model/client/kontur_authorization.py
@@ -1,19 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
 
 from pydantic import BaseModel
-
-
-@dataclass
-class AsyncWeedOperationResponse:
-    status_code: int
-    content: bytes
-    content_type: str
-    headers: dict
-    fid: Optional[str] = None
-    url: Optional[str] = None
-    size: Optional[int] = None
-
 
 @dataclass
 class AuthorizationDataDTO:
